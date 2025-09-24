6424506b688a4668f9d38cb683526517c35b91e2
LuminaStriker <darkmasterofgames@gmail.com>
2025-09-24 12:40:40 -0600

    Miercoles ejercicio commit

--- boceto_2/boceto_2/ui/pantallas/pantalla_3.swift
+++ boceto_2/boceto_2/ui/pantallas/pantalla_3.swift
@@ -7,14 +7,47 @@
 
 import SwiftUI
 
+enum SaboresPalomitas: String, CaseIterable, Identifiable{
+    case Mantequilla
+    case Natural
+    case QuesoCheddar
+    case Doritos
+    case Proteina
+    
+    var id: Self { self }
+}
+
 struct PantallaOpciones: View {
-    @State var valor_slider = 50.0
+    @State var quiere_palomitas: Bool = false
+    @State var sabor_de_palomitas: SaboresPalomitas = .Mantequilla
     
     var body: some View {
         ScrollView{
             VStack{
-                Text("Hola mundo el valor del slider es: \(valor_slider)")
-                Slider(value: $valor_slider, in: 0...100)
+                Text("Formulario para calcular la cantidad de palomitas")
+                HStack{
+                    Spacer()
+                    Text("¿Quieres Palomitas?")
+                    Spacer()
+                    cajita_seleccion(checado: $quiere_palomitas, tamaño: 25)
+                    Spacer()
+                }
+                if(quiere_palomitas){
+                    HStack{
+                        Text("¿Cual Sabor?")
+                        HStack{
+                            Picker(selection: $sabor_de_palomitas, label: Text("Sabor de Palomitas")) {
+                                Text("Mantequilla").tag(SaboresPalomitas.Mantequilla)
+                                Text("Naturales").tag(SaboresPalomitas.Natural)
+                                Text("Cheddar").tag(SaboresPalomitas.QuesoCheddar)
+                                Text("Doritos").tag(SaboresPalomitas.Doritos)
+                                Text("Proteina").tag(SaboresPalomitas.Proteina)
+                            }
+                        }
+                    }
+                }
+//                Text("Hola mundo el valor del slider es: \(valor_slider)")
+//                Slider(value: $valor_slider, in: 0...100)
                 
             }
         }
